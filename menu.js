cc56042cab7fd5141c868cbd449207546ca25400
Tácio Soares <taciofsoares@gmail.com>
2023-06-23 21:18:28 -0300

    Ok

--- menu.js
+++ menu.js
@@ -3,11 +3,11 @@ const menuHamburguer = document.querySelector('#menu-hamburguer')
 
 menuHamburguer.addEventListener('click', () => {
 
-    if (menuHamburguer.style.top == '35px') {
-        menuHamburguer.style.top = '60px'
+    if (menuHamburguer.style.top == '8%') {
+        menuHamburguer.style.top = '12%'
         return menuOpcoes.style.left = '-300px'
     }
-    menuHamburguer.style.top = '35px'
+    menuHamburguer.style.top = '8%'
     return (
         menuOpcoes.style.left = '0px'
     )    
